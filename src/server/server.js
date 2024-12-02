4157b92c536b95d9a82057d76703dd471c2723bd
Pedro Lopes <pedro.hsl2003@gmail.com>
2024-12-02 16:56:07 -0300

    Up Server.js

--- src/server/server.js
+++ src/server/server.js
@@ -32,57 +32,40 @@ function formatDateTime() {
   }).format(now);
 }
 
-// Função para extrair informações do formato MATMSG
-function parseQRCodeData(qrData) {
-  const match = qrData.match(/^MATMSG:TO:(.+);SUB:(.+);BODY:(.+);;$/);
-  if (!match) {
-    throw new Error("Formato inválido de QR Code!");
-  }
-  return {
-    email: match[1],
-    subject: match[2],
-    location: match[3],
-  };
-}
-
 // Endpoint para envio de e-mail
 app.post("/send-email", async (req, res) => {
-  const { qrData } = req.body;
+  const { email } = req.body;
 
-  if (!qrData) {
-    return res.status(400).json({ success: false, message: "Dados do QR Code são obrigatórios!" });
+  if (!email) {
+    return res.status(400).json({ success: false, message: "E-mail é obrigatório!" });
   }
 
-  try {
-    const { email, subject, location } = parseQRCodeData(qrData);
-    const dateTime = formatDateTime(); // Capturar a data e hora no momento do envio
-
-    const mailOptions = {
-      from: process.env.USER_EMAIL,
-      to: email,
-      subject: subject, // Assunto vindo do QR Code
-      text: `Olá,
+  const dateTime = formatDateTime(); // Capturar a data e hora no momento do envio
+  const mailOptions = {
+    from: process.env.USER_EMAIL,
+    to: email,
+    subject: "Confirmação de Visita Técnica",
+    text: `Olá,
 
 A visita técnica foi realizada com sucesso.
 
 Data e Hora: ${dateTime}
-Local: ${location}
 
 Atenciosamente,
 Equipe Técnica`,
-      html: `
-        <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; line-height: 1.6; border: 1px solid #ddd; border-radius: 8px; max-width: 600px; margin: auto;">
-          <h3 style="color: #333; text-align: center; margin-bottom: 20px;">Confirmação de Visita Técnica</h3>
-          <p>Olá,</p>
-          <p>A visita técnica foi realizada com sucesso.</p>
-          <p><strong>Data e Hora:</strong> ${dateTime}</p>
-          <p><strong>Local:</strong> ${location}</p>
-          <p>Atenciosamente,</p>
-          <p><strong>Equipe Técnica</strong></p>
-        </div>
-      `,
-    };
+    html: `
+      <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; line-height: 1.6; border: 1px solid #ddd; border-radius: 8px; max-width: 600px; margin: auto;">
+        <h3 style="color: #333; text-align: center; margin-bottom: 20px;">Confirmação de Visita Técnica</h3>
+        <p>Olá,</p>
+        <p>A visita técnica foi realizada com sucesso.</p>
+        <p><strong>Data e Hora:</strong> ${dateTime}</p>
+        <p>Atenciosamente,</p>
+        <p><strong>Equipe Técnica</strong></p>
+      </div>
+    `,
+  };
 
+  try {
     await transporter.sendMail(mailOptions);
     res.status(200).json({ success: true, message: "E-mail enviado com sucesso!" });
   } catch (error) {
